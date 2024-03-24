90aaa5969faa9adce5dbe81771b2f8725e4d7a34
Amepistheo <kellylhji@gmail.com>
2024-03-24 19:22:41 +0900

    feat: 이미지 퀴즈 UI 1차 제작

---
+++ app/src/main/java/soongsil/kidbean/front/ui/quiz/ImageQuizAddActivity.kt
@@ -0,0 +1,12 @@
+package soongsil.kidbean.front.ui.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import soongsil.kidbean.front.R
+
+class ImageQuizAddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_quiz_add)
+    }
+}
