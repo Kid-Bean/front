51fb00451a8675d871efc0ad8439e6d6442bceff
Amepistheo <kellylhji@gmail.com>
2024-03-26 14:24:55 +0900

    feat: 이미지 문제 API 연결 완료

--- app/src/main/java/soongsil/kidbean/front/data/dto/response/ImageQuizMemberDetailResponse.kt
+++ app/src/main/java/soongsil/kidbean/front/data/dto/response/ImageQuizMemberDetailResponse.kt
@@ -1,7 +1,10 @@
 package soongsil.kidbean.front.data.dto.response
 
+import com.google.gson.annotations.SerializedName
+
 data class ImageQuizMemberDetailResponse(
     val title: String,
+    @SerializedName(value = "imageUrl")
     val imgUrl: String,
     val answer: String
 ) {
