5bf880d72547c867a82dc8787d0bb61781979d29
LDY <leedy3838@naver.com>
2024-04-15 10:12:09 +0900

    fix: 에뮬레이터에서 녹음이 안되던 현상 개선 #4

--- app/src/main/java/soongsil/kidbean/front/quiz/image/dto/response/ImageQuizSolveResponse.kt
+++ app/src/main/java/soongsil/kidbean/front/quiz/image/dto/response/ImageQuizSolveResponse.kt
@@ -3,9 +3,7 @@ package soongsil.kidbean.front.quiz.image.dto.response
 data class ImageQuizSolveResponse(
     val answer: String,
     val s3Url: String,
-    val quizId: Long,
-    val category: String,
-    val title: String
+    val quizId: Long
 ) {
 
 }
