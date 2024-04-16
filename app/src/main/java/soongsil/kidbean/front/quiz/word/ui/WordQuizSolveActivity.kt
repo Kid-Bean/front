2f6cd1a40a70db90c363e11846896276df83e39e
LDY <leedy3838@naver.com>
2024-04-16 14:12:43 +0900

    feat: WordQuiz 정답, 오답 선택 기능 구현 #9

--- app/src/main/java/soongsil/kidbean/front/quiz/word/ui/WordQuizSolveActivity.kt
+++ app/src/main/java/soongsil/kidbean/front/quiz/word/ui/WordQuizSolveActivity.kt
@@ -5,10 +5,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.core.content.ContextCompat
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import soongsil.kidbean.front.MainActivity
+import soongsil.kidbean.front.R
 import soongsil.kidbean.front.databinding.ActivityWordQuizSolveBinding
 import soongsil.kidbean.front.global.ResponseTemplate
 import soongsil.kidbean.front.quiz.QuizListActivity
@@ -20,10 +24,11 @@ class WordQuizSolveActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWordQuizSolveBinding
     private lateinit var title: String
     private lateinit var answer: String
-    private lateinit var word1 : String
-    private lateinit var word2 : String
-    private lateinit var word3 : String
-    private lateinit var word4 : String
+    private lateinit var word1: String
+    private lateinit var word2: String
+    private lateinit var word3: String
+    private lateinit var word4: String
+    private lateinit var selectAnswer: String
     private var quizId: Long = -1L
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,10 +36,11 @@ class WordQuizSolveActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.btnBack.setOnClickListener {
-            val intent = Intent(this, QuizListActivity::class.java)
-            startActivity(intent)
-        }
+        // 단어 버튼에 대한 클릭 리스너 설정
+        setButtonClickListener(binding.btnWord1)
+        setButtonClickListener(binding.btnWord2)
+        setButtonClickListener(binding.btnWord3)
+        setButtonClickListener(binding.btnWord4)
 
         loadInfo()
         bottomSetting()
@@ -98,6 +104,7 @@ class WordQuizSolveActivity : AppCompatActivity() {
 
                     // API로 가져온 정답 넣기
                     answer = body.answer
+                    binding.btnAnswer.text = answer
 
                     Log.d("text1", binding.btnWord1.text.toString())
                     Log.d("text2", binding.btnWord2.text.toString())
@@ -112,10 +119,40 @@ class WordQuizSolveActivity : AppCompatActivity() {
                 }
             }
 
-            override fun onFailure(call: Call<ResponseTemplate<WordQuizSolveResponse>>, t: Throwable) {
+            override fun onFailure(
+                call: Call<ResponseTemplate<WordQuizSolveResponse>>,
+                t: Throwable
+            ) {
                 // 통신 실패 (인터넷 끊킴, 예외 발생 등 시스템적인 이유)
                 Log.d("post", "onFailure 에러: " + t.message.toString())
             }
         })
+    }
+
+    private fun setButtonClickListener(button: Button) {
+        button.setOnClickListener {
+            // 선택된 답변을 변수에 저장
+            selectAnswer = button.text.toString()
+
+            // 정답 확인
+            if (selectAnswer == answer) {
+                // 정답일 경우 배경색을 파란색으로 변경
+                button.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.green)
+            } else {
+                // 오답일 경우 이미 설정된 빨간색 배경을 유지
+                button.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.red)
+            }
+            button.setTextColor(ContextCompat.getColor(applicationContext, R.color.white))
+
+            // 다른 버튼들을 비활성화
+            val buttons = listOf(binding.btnWord1, binding.btnWord2, binding.btnWord3, binding.btnWord4)
+            for (btn in buttons) {
+                if (btn != button) { // 클릭된 버튼을 제외한 나머지 버튼
+                    btn.isEnabled = false // 버튼 비활성화
+                }
+            }
+
+            binding.btnAnswer.visibility = View.VISIBLE
+        }
     }
 }
