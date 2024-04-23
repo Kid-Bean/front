ee5de8d94de2c20eb559e234ba228d5d3b347b5d
Amepistheo <kellylhji@gmail.com>
2024-04-23 23:27:27 +0900

    feat: 질문 문제 수정 기능 구현 #8

--- app/src/main/java/soongsil/kidbean/front/quiz/answer/presentation/AnswerQuizController.kt
+++ app/src/main/java/soongsil/kidbean/front/quiz/answer/presentation/AnswerQuizController.kt
@@ -1,10 +1,15 @@
 package soongsil.kidbean.front.quiz.answer.presentation
 
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 import soongsil.kidbean.front.global.ResponseTemplate
+import soongsil.kidbean.front.quiz.answer.dto.request.AnswerQuizUpdateRequest
+import soongsil.kidbean.front.quiz.answer.dto.request.AnswerQuizUploadRequest
 import soongsil.kidbean.front.quiz.answer.dto.response.AnswerQuizMemberDetailResponse
 import soongsil.kidbean.front.quiz.answer.dto.response.AnswerQuizMemberResponse
 
@@ -21,9 +26,24 @@ interface AnswerQuizController {
         @Path("quizId") quizId: Long
     ): Call<ResponseTemplate<AnswerQuizMemberDetailResponse>>
 
+    @POST("quiz/answer/member/{memberId}")
+    fun uploadAnswerQuiz(
+        @Path("memberId") memberId: Long,
+        @Body request: AnswerQuizUploadRequest
+    ): Call<ResponseTemplate<Void>>
+
+    @PUT("quiz/answer/member/{memberId}/{quizId}")
+    fun updateAnswerQuiz(
+        @Path("memberId") memberId: Long,
+        @Path("quizId") quizId: Long,
+        @Body request: AnswerQuizUpdateRequest
+    ): Call<ResponseTemplate<Void>>
+
     @DELETE("quiz/answer/member/{memberId}/{quizId}")
     fun deleteAnswerQuiz(
         @Path("memberId") memberId: Long,
         @Path("quizId") quizId: Long
     ) :Call<ResponseTemplate<Void>>
+
+
 }
---
+++ app/src/main/java/soongsil/kidbean/front/quiz/answer/ui/AnswerQuizUploadActivity.kt
@@ -0,0 +1,12 @@
+package soongsil.kidbean.front.quiz.answer.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import soongsil.kidbean.front.R
+
+class AnswerQuizUploadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_answer_quiz_upload)
+    }
+}
