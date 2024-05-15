ce26f373fd07a77c9b5769e70ff2cb322e6f4f36
LDY <leedy3838@naver.com>
2024-05-15 11:09:05 +0900

    feat: 프로그램 리스트 조회 화면 구현 #26

--- app/src/main/java/soongsil/kidbean/front/program/presentation/ProgramController.kt
+++ app/src/main/java/soongsil/kidbean/front/program/presentation/ProgramController.kt
@@ -1,4 +1,16 @@
 package soongsil.kidbean.front.program.presentation
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+import soongsil.kidbean.front.global.ResponseTemplate
+import soongsil.kidbean.front.program.dto.response.ProgramResponseList
+
 interface ProgramController {
+
+    @GET("programs")
+    fun getProgramList(
+        @Query("programcategory") programCategory: String,
+        @Query("page") page: Int
+    ): Call<ResponseTemplate<ProgramResponseList>>
 }
--- app/src/main/java/soongsil/kidbean/front/program/ui/ProgramAdapter.kt
+++ app/src/main/java/soongsil/kidbean/front/program/ui/ProgramAdapter.kt
@@ -3,8 +3,13 @@ package soongsil.kidbean.front.program.ui
 import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import soongsil.kidbean.front.R
+import soongsil.kidbean.front.databinding.ItemProgramBinding
 import soongsil.kidbean.front.databinding.ItemQuizBinding
 import soongsil.kidbean.front.program.dto.response.ProgramResponse
 import soongsil.kidbean.front.quiz.image.ui.ImageQuizShowActivity
@@ -12,28 +17,41 @@ import soongsil.kidbean.front.quiz.image.ui.ImageQuizShowActivity
 class ProgramAdapter(private val dataList: List<ProgramResponse>) :
     RecyclerView.Adapter<ProgramAdapter.ViewHolder>() {
 
-    inner class ViewHolder(private val binding: ItemQuizBinding) :
+    inner class ViewHolder(private val binding: ItemProgramBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(position: Int) {
-//            binding.tvQuizTitle.text = dataList[position].
+            binding.tvProgramName.text = dataList[position].programTitle
+            binding.tvDepartmentName.text = dataList[position].departmentName
+            binding.tvProgramPlace.text = dataList[position].place
+
+            //isStar 가 true -> 노란 별 이미지
+            if (dataList[position].isStar) {
+                binding.imgStar.setImageResource(R.drawable.star)
+            }
+
+            // API로 가져온 이미지 넣기
+            val imageView: ImageView = binding.imgDepartment
+
+            Glide.with(this.itemView.context)
+                .load(dataList[position].departmentS3Url)
+                .into(imageView)
+
+            imageView.visibility = View.VISIBLE
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val viewBinding =
-            ItemQuizBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val viewBinding = ItemProgramBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(viewBinding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val currentQuiz = dataList[position]
         holder.bind(position)
 
         holder.itemView.setOnClickListener {
-            val intent = Intent(it.context, ImageQuizShowActivity::class.java)
-//            intent.putExtra("quizId", currentQuiz.quizId)
-//            Log.d("imagequiz", currentQuiz.quizId.toString())
+            val intent = Intent(it.context, ProgramDetailActivity::class.java)
+            intent.putExtra("programId", dataList[position].programId)
             it.context.startActivity(intent)
         }
     }
